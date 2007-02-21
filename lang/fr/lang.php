8faeaa95241f3a02183dd0417b5e55186a58d381
wikidesign <chi@chimeric.de>
2007-02-21 14:29:28 +0100

    15 French translation by Eric Guirbal
    darcs-hash:20070221132928-89468-dc17b62feee33f5784e9ce2945a47a86a9043bec.gz

---
+++ lang/fr/lang.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * French language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Joseph Diniz <joseph.diniz@mac.com>
+ */
+
+// custom language strings for the plugin
+$lang['task']     = 'Tache';
+$lang['tasks']    = 'taches';
+$lang['newtask']  = 'nouvelle tache';
+$lang['user']     = 'affectée à';
+$lang['date']     = 'Date limite';
+
+$lang['priority'] = 'priorité';
+$lang['low']      = 'faible';
+$lang['medium']   = 'moyenne';
+$lang['high']     = 'élevé';
+$lang['critical'] = 'critique';
+
+$lang['status']   = 'Statut';
+$lang['rejected'] = 'rejetée';
+$lang['new']      = 'nouvelle';
+$lang['accepted'] = 'acceptée';
+$lang['started']  = 'démarrée';
+$lang['done']     = 'fait';
+$lang['verified'] = 'verfiée';
+
+$lang['mail_newtask']      = 'ajout de nouvelle tache';
+$lang['mail_changedtask']  = 'statut de la tache modifié';
+
+$lang['view']         = 'Visualiser les taches:';
+$lang['view_all']     = 'tous';
+$lang['view_open']    = 'oovert';
+$lang['view_my']      = 'ma';
+$lang['view_new']     = 'nouvelle';
+$lang['view_done']    = 'fait';
+$lang['view_due']     = 'attendue';
+$lang['view_overdue'] = 'dépassée';
+
+$lang['btn_change']   = 'Change';
+$lang['btn_refresh']  = 'Actualiser';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
