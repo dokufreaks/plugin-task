e93a47f8a09e95bd34d34d4e448201250a02ea4c
wikidesign <chi@chimeric.de>
2007-07-21 10:43:55 +0200

    17 Russian translation by Алексей Лёвин
    darcs-hash:20070721084355-89468-04f230b997d710fd7e657653175e650d308b505f.gz

---
+++ lang/ru/lang.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Russian language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Алексей Лёвин <alexlevin@kvadro.net>
+ */
+
+// custom language strings for the plugin
+$lang['task']     = 'Задача';
+$lang['tasks']    = 'Задачи';
+$lang['newtask']  = 'Новая задача';
+$lang['user']     = 'Ответственный';
+$lang['date']     = 'Срок до';
+
+$lang['priority'] = 'Приоритет';
+$lang['low']      = 'низкий';
+$lang['medium']   = 'средний';
+$lang['high']     = 'высокий';
+$lang['critical'] = 'критический';
+
+$lang['status']   = 'Статус';
+$lang['rejected'] = 'отклонена';
+$lang['new']      = 'новая';
+$lang['accepted'] = 'принята';
+$lang['started']  = 'начата';
+$lang['done']     = 'выполнена';
+$lang['verified'] = 'сверена';
+
+$lang['mail_newtask']      = 'Добавлена новая задача';
+$lang['mail_changedtask']  = 'Изменен статус задачи';
+
+$lang['view']         = 'Показать задачи:';
+$lang['view_all']     = 'все';
+$lang['view_open']    = 'открытые';
+$lang['view_my']      = 'мои';
+$lang['view_new']     = 'новые';
+$lang['view_done']    = 'выполненные';
+$lang['view_due']     = 'в срок';
+$lang['view_overdue'] = 'просрочено';
+
+$lang['btn_change']   = 'Изменить';
+$lang['btn_refresh']  = 'Обновить';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
---
+++ lang/ru/settings.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Russian language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Алексей Лёвин <alexlevin@kvadro.net>
+ */
+
+// for the configuration manager
+$lang['datefield']                   = 'Показывать поле для срока задачи';
+
+$lang['tasks_formposition']          = 'Положение формы в новой задаче';
+$lang['tasks_formposition_o_none']   = 'нет';
+$lang['tasks_formposition_o_top']    = 'сверху';
+$lang['tasks_formposition_o_bottom'] = 'снизу';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
