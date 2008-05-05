c058b541fd956fddc4ce03b41254350a93341420
Michael Klier <chi@chimeric.de>
2008-05-05 17:25:01 +0200

    added spanish translation
    darcs-hash:20080505152501-23886-d80dde1da844699e00bbd7bcb41e0e29b3dab89f.gz

---
+++ lang/es/lang.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Spanish language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Agustín Herranz <djpalanka at gmail dot com>
+ */
+
+// custom language strings for the plugin
+$lang['task']     = 'Tarea';
+$lang['tasks']    = 'Tareas';
+$lang['newtask']  = 'Nueva Tarea';
+$lang['user']     = 'Asignado a';
+$lang['date']     = 'Vencimiento';
+
+$lang['priority'] = 'Prioridad';
+$lang['low']      = 'baja';
+$lang['medium']   = 'media';
+$lang['high']     = 'alta';
+$lang['critical'] = 'critica';
+
+$lang['status']   = 'Estado';
+$lang['rejected'] = 'rechazado';
+$lang['new']      = 'nuevo';
+$lang['accepted'] = 'aceptado';
+$lang['started']  = 'iniciado';
+$lang['done']     = 'completado';
+$lang['verified'] = 'verificado';
+
+$lang['mail_newtask']      = 'Nueva tarea añadida';
+$lang['mail_changedtask']  = 'Estado de tarea modificado';
+
+$lang['view']         = 'mostrar tareas:';
+$lang['view_all']     = 'todas';
+$lang['view_open']    = 'abiertas';
+$lang['view_my']      = 'mias';
+$lang['view_new']     = 'nuevas';
+$lang['view_done']    = 'completadas';
+$lang['view_due']     = 'caduca hoy';
+$lang['view_overdue'] = 'caducadas';
+
+$lang['btn_change']   = 'Cambiar';
+$lang['btn_refresh']  = 'Refrescar';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
---
+++ lang/es/settings.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Spanish language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Agustín Herranz <djpalanka at gmail dot com>
+ */
+
+// for the configuration manager
+$lang['datefield']                   = 'campo para la fecha de vencimiento';
+
+$lang['tasks_formposition']          = 'posición del cuadro Nueva Tarea';
+$lang['tasks_formposition_o_none']   = 'ninguna';
+$lang['tasks_formposition_o_top']    = 'arriba';
+$lang['tasks_formposition_o_bottom'] = 'debajo';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
