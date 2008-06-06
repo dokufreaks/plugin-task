4b5aa785fdee842275f64d491e027e809d5acc50
Michael Klier <chi@chimeric.de>
2008-06-06 16:11:40 +0200

    fix for FS#40
    darcs-hash:20080606141140-23886-c3c55af74e5205dfcdedf01fbe96a4dcf4687c43.gz

--- ics.php
+++ ics.php
@@ -17,17 +17,23 @@ $id = $_REQUEST['id'];
 $data = unserialize(io_readFile(metaFN($id, '.task'), false));
 if (!$data['vtodo']) msg('No VTODO data for this task.', -1);
 
-$filename = p_get_metadata($id, 'title').'.ics';
+$title = p_get_metadata($id, 'title');
+if($title) {
+    $filename = $title . '.ics';
+} else {
+    $filename = str_replace(':', '/', cleanID($id)) . '.ics';
+}
+
 $output = 'BEGIN:VCALENDAR'.CRLF.
   'PRODID:-//Wikidesign//NONSGML Task Plugin for DokuWiki//EN'.CRLF.
   'VERSION:2.0'.CRLF.
   $data['vtodo'].
   'END:VCALENDAR'.CRLF;
 
-header('Content-Disposition: attachment; filename='.$filename);
+header("Content-Disposition: attachment; filename='$filename'");
 header('Content-Length: '.strlen($output));
 header('Connection: close');
-header('Content-Type: text/Calendar; name='.$filename);
+header("Content-Type: text/Calendar; name='$filename'");
 
 echo $output;
 
