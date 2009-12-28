c574e56d4592a7e8969fea30866bb6b9ddacade3
Sidnei Francisco Neves <sdnnvs@gmail.com>
2009-12-28 13:44:44 +0100

    added Portuguese of Brazil translation

---
+++ lang/pt-br/lang.php
@@ -0,0 +1,46 @@
+﻿<?php
+/**
+ * Arquivos de língua portuguesa do Brasil
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Sidnei Francisco Neves <sdnnvs@gmail.com>
+ */
+
+// custom language strings for the plugin
+$lang['task']     = 'Tarefa';
+$lang['tasks']    = 'Tarefas';
+$lang['newtask']  = 'Nova tarefa';
+$lang['user']     = 'Atribuída a';
+$lang['date']     = 'Meta';
+
+$lang['priority'] = 'Prioridade';
+$lang['low']      = 'baixa';
+$lang['medium']   = 'média';
+$lang['high']     = 'alta';
+$lang['critical'] = 'crítica';
+
+$lang['status']   = 'Status';
+$lang['rejected'] = 'rejeitada';
+$lang['new']      = 'nova';
+$lang['accepted'] = 'aceita';
+$lang['started']  = 'iniciada';
+$lang['done']     = 'finalizada';
+$lang['verified'] = 'verificada';
+
+$lang['mail_newtask']      = 'Nova tarefa adicionada';
+$lang['mail_changedtask']  = 'Status da tarefa alterado';
+
+$lang['view']         = 'Mostrar tarefas:';
+$lang['view_all']     = 'todas';
+$lang['view_open']    = 'aberta';
+$lang['view_my']      = 'minhas';
+$lang['view_new']     = 'novas';
+$lang['view_done']    = 'finalizadas';
+$lang['view_due']     = 'no prazo';
+$lang['view_overdue'] = 'prazo esgotado';
+$lang['view_user']    = 'Mostrar apenas as minhas tarefas';
+
+$lang['btn_change']   = 'Mudar';
+$lang['btn_refresh']  = 'Atualizar';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
---
+++ lang/pt-br/settings.php
@@ -0,0 +1,19 @@
+﻿<?php
+/**
+ * Arquivo de língua portuguesa do Brasil
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Sidnei Francisco Neves <sdnnvs@gmail.com>
+ */
+
+// for the configuration manager
+$lang['datefield']                   = 'entrada de data';
+
+$lang['tasks_formposition']          = 'posição do novo formulário';
+$lang['tasks_formposition_o_none']   = 'nenhum';
+$lang['tasks_formposition_o_top']    = 'encima';
+$lang['tasks_formposition_o_bottom'] = 'embaixo';
+
+$lang['tasks_newewstfirst']          = 'mostrar primeiro as tarefas recentes na lista';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
